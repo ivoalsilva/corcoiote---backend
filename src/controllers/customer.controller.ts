4171bd18feb4973b48302f62628953435a62d4f4
ivo alsivla <ivoalsilva@hotmail.com>
2026-07-17 21:43:18 -0300

    feat: estabelecer camada de controlador para recurso de clientes

---
+++ src/controllers/customer.controller.ts
@@ -0,0 +1,9 @@
+import type { Request, Response } from 'express'
+import { findAllCustomer } from '../services/customer.service.ts'
+
+export function getAllCustomers(
+    _request: Request,
+    response: Response) {
+    const customers = findAllCustomer()
+    response.status(200).json(customers);
+}
