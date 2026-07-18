c6af063cc3902ca5ad8b5a39a81301cb515b3623
ivo alsivla <ivoalsilva@hotmail.com>
2026-07-17 21:40:37 -0300

    mover dados simulados para diretorio dedicado

---
+++ src/mocks/customer.mock.ts
@@ -0,0 +1,18 @@
+import type { Customer } from "../types.ts"; 
+
+const customers : Customer[] = [{
+        id: 1,
+        name: 'ivo',
+        status: true
+    }, {
+        id: 2,
+        name: 'arthur',
+        status: true
+    }, {
+        id: 3,
+        name: 'davi',
+        status: false
+    }];
+
+
+    export default customers;
