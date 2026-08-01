3ab62985d3e3d5fcde0f5f2ff00b619c07e61e12
ivo alsivla <ivoalsilva@hotmail.com>
2026-07-31 21:37:50 -0300

    feat: adicionar intermediarios para registro de requisicoes e manipulacao de erros

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,14 @@
 import express from 'express';
 import customerRouter from './routes/customer.route.ts';
+import requestLogger from './middlewares/requestLogger.ts';
+import errorHandler from './middlewares/errorHandler.ts';
 
 const app = express()
 
 app.use(express.json());
 
 app.use('/customers' , customerRouter);
+
+app.use(errorHandler);
 
 app.listen(3000);
